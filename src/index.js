000fddfdb117a7c35efc25d2b73556798698854b
Toxa-K <originkewai666.com@mail.ru>
2024-11-12 13:27:35 +0300

    add translate library i18next for my bot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const dotenv = require ('dotenv');
 require('dotenv').config();
-const {Telegraf} = require('telegraf');
+
+const {Telegraf} = require('telegraf');//https://telegraf.js.org/index.html
 const { message } = require('telegraf/filters');
 const {BOT_TOKEN} = process.env;
-
+const i18next = require('i18next'); //https://www.i18next.com/overview/getting-started
 
 const bot = new Telegraf(BOT_TOKEN);
+
+i18next.init({
+    lng: 'ru', // if you're using a language detector, do not define the lng option
+    debug: true,
+    resources: {
+      en: {
+        translation: {
+          "key": "привет мир"
+        }
+      }
+    }
+});
+
+
 bot.start((ctx)=>ctx.reply('Welcome!'))
 bot.help((ctx)=>ctx.reply('Send me a sticker'));
 bot.on(message('sticker'), (ctx)=>ctx.reply('👍'))
