6c0c8276bca03c85b5866e8593de406300678f17
Toxa-K <originkewai666.com@mail.ru>
2024-11-12 12:48:44 +0300

    add bot's function for reply

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const dotenv = require ('dotenv');
 require('dotenv').config();
 const {Telegraf} = require('telegraf');
+const { message } = require('telegraf/filters');
 const {BOT_TOKEN} = process.env;
 
 
 const bot = new Telegraf(BOT_TOKEN);
+bot.start((ctx)=>ctx.reply('Welcome!'))
+bot.help((ctx)=>ctx.reply('Send me a sticker'));
+bot.on(message('sticker'), (ctx)=>ctx.reply('👍'))
+bot.hears((message), (ctx)=> ctx.reply('Hello,body!'));
+
 
 bot.launch();
